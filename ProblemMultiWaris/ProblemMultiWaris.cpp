215aaa126b40e50e9062f91b6209a4d87d9880a8
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-05-29 10:52:52 +0700

    membuat kelas budi

--- ProblemMultiWaris/ProblemMultiWaris.cpp
+++ ProblemMultiWaris/ProblemMultiWaris.cpp
@@ -31,4 +31,13 @@ public:
 	}
 };
 
-class
+class budi : public pekerja, public pelajar {
+public:
+	 
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "Budi dibuat\n" << endl;
+	}
+};
