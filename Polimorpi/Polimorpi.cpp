8c544c18e75fcac9d1c5a8ac5be12c2c7077452d
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-05-29 11:18:22 +0700

    kelas joko

--- Polimorpi/Polimorpi.cpp
+++ Polimorpi/Polimorpi.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
-class seorang {
+class seseorang {
 public:
 	virtual void pesan() = 0;
 	// virtual void pesan(){
 	//cout<<"pesan dari seseorang"<<endl;
 	//}
+};
+
+class joko :public seseorang {
+public:
+	void pesan() {
+		cout << "pesan dari joko" << endl;
+	}
 };
